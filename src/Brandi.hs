848c043adbc012b116604b10a75a0a5ea36578ca
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-04-10 23:29:10 +0530

    feat: pretty much done
    - create tags page
    - create categories page
    - index.html is actually dynamic
    - support for pretty URLs (i.e posts are rendered as post/index.html)
    
    TODO:
    - prettify links; they show up as `post/html` instead of just `post/`
    - an archive page to list all posts
    - pagination perhaps?
    - cleanup the code a bit more

--- src/Brandi.hs
+++ src/Brandi.hs
@@ -13,8 +13,48 @@ import           Hakyll
 import           Hakyll.Images     (compressJpgCompiler, loadImage)
 import           Hakyll.Web.Sass   (sassCompilerWith)
 import           RIO               hiding (handle)
+import           RIO.List          (headMaybe)
+import           System.FilePath   (combine, dropExtension, joinPath,
+                                    splitDirectories)
 import           Text.Sass.Options (SassOptions (..), SassOutputStyle (..),
                                     defaultSassOptions)
+
+--------------------------------------------------------------------------------
+
+indexHtmlRoute :: Routes
+indexHtmlRoute = customRoute $ flip combine "index.html" . dropExtension . toFilePath
+
+rootlessRoute :: Routes
+rootlessRoute = customRoute $ joinPath . drop 1 . splitDirectories . toFilePath
+
+categoryName :: Item a -> Compiler String
+categoryName (Item i _) = return $ getL1Category  i
+
+createTagsPages :: Tags -> Context String -> Rules ()
+createTagsPages tags baseCtx = tagsRules tags $
+  \tag ptrn -> do
+    let title = tag
+
+    route indexHtmlRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll ptrn
+      let tagsCtx =
+            constField "title" title
+            <> listField "posts" baseCtx (return posts)
+            <> constField "tag" tag
+            <> baseCtx
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tag.html" tagsCtx
+        >>= loadAndApplyTemplate "templates/default.html" tagsCtx
+        >>= relativizeUrls
+
+getL1Category :: Identifier -> String
+getL1Category = fromMaybe "uncategorized" . headMaybe . drop 1 . splitDirectories . dropExtension . toFilePath
+
+getL1Category' :: MonadMetadata m => Identifier -> m [String]
+getL1Category' = return . return . getL1Category
+
 --------------------------------------------------------------------------------
 
 type URL = Text
@@ -28,26 +68,29 @@ data BlogConfig = BlogConfig
   , stackoverflow :: URL
   , gpgPublicQr   :: URL
   , resume        :: URL
+  , homeTitle     :: Text
   } deriving (Generic, Show)
 
 instance Interpret BlogConfig
 
 
 config :: Configuration
-config = defaultConfiguration { destinationDirectory = "build/site"
-                              , storeDirectory       = "build/_store"
-                              , tmpDirectory         = "build/_tmp"
-                              }
+config = defaultConfiguration
+  { destinationDirectory = "build/site"
+  , storeDirectory       = "build/_store"
+  , tmpDirectory         = "build/_tmp"
+  }
 
 sassOptions :: SassOptions
 sassOptions = defaultSassOptions
-              { sassSourceMapEmbed = True
-    , sassOutputStyle    = SassStyleCompressed
-    }
+  { sassSourceMapEmbed = True
+  , sassOutputStyle    = SassStyleCompressed
+  }
 
 blogConfigCtx :: BlogConfig -> Context String
 blogConfigCtx conf =
-  pickAs "github" github
+  pickAs "homeTitle" homeTitle
+  <> pickAs "github"          github
   <> pickAs "name"          name
   <> pickAs "handle"        handle
   <> pickAs "linkedin"      linkedin
@@ -60,31 +103,42 @@ blogConfigCtx conf =
     pick a = unpack . a $ conf
     pickAs s a = constField s (pick a)
 
-postsGlob :: Pattern
-postsGlob = "content/**.md"
+contentGlob :: Pattern
+contentGlob = "content/**.md"
 
 jpgsGlob :: Pattern
 jpgsGlob = "images/**.jpg"
 
-postCtx :: Context String
-postCtx =  dateField "date" "%B %e, %Y"
-        <> constField "item-type" "post"
-        <> pathField "sourcefile"
+postCtx' :: BlogConfig -> Tags -> Tags -> Context String
+postCtx' conf categories tags =  dateField "date" "%B %e, %Y"
+        <> field "category" categoryName
+        <> categoryField "categoryHtml" categories
+        <> tagsField "tagsHtml" tags
+        <> blogConfigCtx conf
         <> defaultContext
+
 
 run :: IO ()
 run = do
   blogConf :: BlogConfig <- input auto "./config.dhall"
 
   hakyllWith config $ do
+    categories <- buildTagsWith getL1Category' contentGlob (fromCapture "*.html")
+    tags <- buildTags contentGlob (fromCapture "tags/*.html")
+
+    let postCtx = postCtx' blogConf categories tags
+
     -- compress images
     match jpgsGlob $ do
       route idRoute
       compile $ loadImage
         >>= compressJpgCompiler 50
 
+    -- inform hakyll about templates
+    match "templates/**.html" $ compile templateCompiler
+
     -- copy assets (non images and non post files)
-    match ("content/**" .&&. complement postsGlob .&&. complement jpgsGlob) $ do
+    match ("content/**" .&&. complement contentGlob .&&. complement jpgsGlob) $ do
       route idRoute
       compile copyFileCompiler
 
@@ -100,18 +154,31 @@ run = do
         route $ setExtension "css"
         compile sassCompiler
 
+    -- assemble content
+    match contentGlob $ do
+      let ctx = postCtx
+      route $ composeRoutes indexHtmlRoute rootlessRoute
+      compile $
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+    -- assemble tag and category pages
+    createTagsPages tags postCtx
+    createTagsPages categories postCtx
+
     -- Create index.html
     match "index.html" $ do
       route idRoute
       compile $ do
-        posts <- recentFirst =<< loadAll postsGlob
-        let indexCtx = listField "posts" postCtx (return posts)
-              <> constField "title" "Home"
+        posts <- recentFirst =<< loadAll contentGlob
+        let indexCtx = listField "posts" postCtx (return $ take 5 posts)
+              <> constField "isHome" "yes"
               <> blogConfigCtx blogConf
               <> defaultContext
 
         getResourceBody
             >>= applyAsTemplate indexCtx
+            >>= loadAndApplyTemplate "templates/default.html" indexCtx
             >>= relativizeUrls
-
---------------------------------------------------------------------------------
