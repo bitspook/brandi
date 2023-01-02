6fb561dfd65caf16d86460662273c13b75fe78c2
Charanjit Singh <charanjit.singh@on-running.com>
2023-01-02 23:24:42 +0100

    feat: very basic UI for github feed
    Time to bring in some real github events to gauge what we are working with.

--- src/spa/src/views/Home/index.tsx
+++ src/spa/src/views/Home/index.tsx
@@ -1,5 +1,23 @@
 import * as styles from './styles.module.scss';
 
 export default () => {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div>
+      <nav className={styles.topNav}>
+        <div className={styles.brand}>Brandi</div>
+      </nav>
+      <div className={styles.container}>
+        <div className={styles.feed}>
+          <section className={styles.event}>
+            <header className={styles.eventTitle}>
+              @klnegi pushed to ullu
+            </header>
+            <article className={styles.eventBody}>
+              Some really awesome code.
+            </article>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/spa/src/views/Home/styles.module.scss.d.ts
+++ src/spa/src/views/Home/styles.module.scss.d.ts
@@ -1,1 +1,7 @@
 export const container: string;
+export const topNav: string;
+export const brand: string;
+export const feed: string;
+export const event: string;
+export const eventTitle: string;
+export const eventBody: string;
